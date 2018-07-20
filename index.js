11b8c6db4aad7de6b3700c188b4e868ef2d9bbb9
Acharya Raja Sekhar Brundhavanam <acharya.r@live.com>
2018-07-20 16:09:47 +0530

    issue fix

--- index.js
+++ index.js
@@ -1,45 +1,42 @@
-const 
+const
     express = require('express'),
     { urlencoded, json } = require('body-parser'),
     cors = require('cors'),
     request = require('request');
-    
+
 const app = express();
 app.use(cors());
 app.use(urlencoded({ extended: false }));
 app.use(json());
 
 app.get('/', (req, res) => {
-   res.send('I am running...');
+    res.send('I am running...');
 })
 
 app.post('/get-user-by-id', (req, res) => {
     console.log(req.body);
 
     if (isNaN(req.body.queryResult.parameters.user)) {
+        return res.json({
+            fulfillmentText: "Seems you have provided invalid user id '" + req.body.queryResult.parameters.user + "'. Please try again."
+        });
+    }
+    else {
         request
             .get('https://reqres.in/api/users/' + req.body.queryResult.parameters.user)
-            .on('response', function(response) {
+            .on('response', function (response) {
                 console.log(response);
                 return res.json({
                     fulfillmentText: JSON.stringify(response.body)
                 });
             })
-            .on('error', function(err) {
+            .on('error', function (err) {
                 console.log(err);
                 return res.json({
                     fulfillmentText: "I am not able contact user's API"
                 });
-            })
+            });
     }
-    else {
-        return res.json({
-            fulfillmentText: "Seems you have provided invalid user id '" + req.body.queryResult.parameters.user + "'. Please try again."
-        });
-    }
-
-    
-    
 })
 
 const server = app.listen(process.env.PORT, () => {
