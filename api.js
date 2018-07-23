42d54208f6e831c18413d344ef83561aff0323f1
Acharya Raja Sekhar Brundhavanam <acharya.r@live.com>
2018-07-23 14:19:21 +0530

    test

--- api.js
+++ api.js
@@ -16,52 +16,60 @@ module.exports = (req, res) => {
 
         return res.json(rrr);
     }
-
-
-    if (isNaN(req.body.queryResult.parameters.userid)) {
-        return res.json({
-            fulfillmentText: "Seems you have provided invalid user id '" + req.body.queryResult.parameters.userid + "'. Please try again."
+    else if(req.body.queryResult.intent.displayName == "get.order.status")
+    {
+        var rrr = res.json({
+            fulfillmentText: "Here is the order status"
         });
-    }
-    else {
-        var options = {
-            method: 'GET',
-            uri: 'https://reqres.in/api/users/' + req.body.queryResult.parameters.userid,
-            json: true
-        };
 
-        rp(options)
-            .then(response => {
-                console.log(response);
-                if(response && response.data) {
-                    return res.json({
-                        fulfillmentText: "The requested user name is " + response.data.first_name + " " + response.data.last_name
-                    });
-                }
-                else {
-                    return res.json({
-                        fulfillmentText: "Could't locate the requested user details from our database..."
-                    });
-                }
-            })
-            .catch(rp_errors.StatusCodeError, reason => {
-                console.log(reason.statusCode);
-                if(reason.statusCode === 404) {
-                    return res.json({
-                        fulfillmentText: "Could't locate the requested user details from our database..."
-                    });
-                }
-                else {
-                    return res.json({
-                        fulfillmentText: "Something went wrong while contacting the backed servers. Please contact the support team..."
-                    });
-                }
-            })
-            .catch(rp_errors.RequestError, function (reason) {
-                console.log(reason.cause);
-                return res.json({
-                    fulfillmentText: "Something went wrong while contacting the backed servers. Please contact the support team..."
-                });
-            });
+        return res.json(rrr);
     }
+
+
+    // if (isNaN(req.body.queryResult.parameters.userid)) {
+    //     return res.json({
+    //         fulfillmentText: "Seems you have provided invalid user id '" + req.body.queryResult.parameters.userid + "'. Please try again."
+    //     });
+    // }
+    // else {
+    //     var options = {
+    //         method: 'GET',
+    //         uri: 'https://reqres.in/api/users/' + req.body.queryResult.parameters.userid,
+    //         json: true
+    //     };
+
+    //     rp(options)
+    //         .then(response => {
+    //             console.log(response);
+    //             if(response && response.data) {
+    //                 return res.json({
+    //                     fulfillmentText: "The requested user name is " + response.data.first_name + " " + response.data.last_name
+    //                 });
+    //             }
+    //             else {
+    //                 return res.json({
+    //                     fulfillmentText: "Could't locate the requested user details from our database..."
+    //                 });
+    //             }
+    //         })
+    //         .catch(rp_errors.StatusCodeError, reason => {
+    //             console.log(reason.statusCode);
+    //             if(reason.statusCode === 404) {
+    //                 return res.json({
+    //                     fulfillmentText: "Could't locate the requested user details from our database..."
+    //                 });
+    //             }
+    //             else {
+    //                 return res.json({
+    //                     fulfillmentText: "Something went wrong while contacting the backed servers. Please contact the support team..."
+    //                 });
+    //             }
+    //         })
+    //         .catch(rp_errors.RequestError, function (reason) {
+    //             console.log(reason.cause);
+    //             return res.json({
+    //                 fulfillmentText: "Something went wrong while contacting the backed servers. Please contact the support team..."
+    //             });
+    //         });
+    // }
 }
